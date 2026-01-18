4558323dc1f4de3e1d8e7251e06232e64483e24b
Arcanox <arcanox@arcanox.me>
2026-01-18 15:35:09 -0600

    Add some helper Dread types (for reading game strings)

--- source/program/dread_types.hpp
+++ source/program/dread_types.hpp
@@ -8,3 +8,26 @@ typedef struct {
     ptrdiff_t luaRegisterGlobals;
     ptrdiff_t lua_pcall;
 } functionOffsets;
+
+typedef unsigned long long crc64_t;
+
+struct CRntString {
+	char* str;
+	int length;
+	void* allocator;
+	bool usesMainAllocator;
+	crc64_t hash;
+	bool isEmpty;
+};
+
+struct CStringInstance {
+	void* stringPoolEntry;
+	unsigned int uses;
+	CRntString string;
+	bool storeInPool;
+	unsigned int unknown;
+};
+
+struct CStrId {
+	CStringInstance *value;
+};
